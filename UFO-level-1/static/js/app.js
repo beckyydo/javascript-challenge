5d1c25a98e24cbfdecf78af1fb5fc562cd1d34e9
beckyydo <47190773+beckyydo@users.noreply.github.com>
2021-01-12 21:12:12 -0500

    filter data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,16 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select button and form
+var button = d3.select("#filter-btn");
+
+function ufo_filter() {
+  
+    // Select the input element and get the raw HTML node
+    var input_date = d3.select("#datetime").text();
+
+    console.log(input_date);
+    console.log(tableData);
+};
+
+button.on("click", ufo_filter);
